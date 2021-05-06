b7af31ea2af435a2119a974a7af41d6cde108de6
Maxime Moilliet <mllt.dev66@gmail.com>
2021-05-06 15:45:40 +0200

    feat(Footer) create Footer Component

--- src/Components/Base/Footer.js
+++ src/Components/Base/Footer.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export function Footer () {
     return (
         <footer className="footer">
-            gfyhth
+            <Link to="/">
+                <img className="footer__logo" src="./images/logo-dark.png" alt="Logo Kasa"/>
+            </Link>
+            <p className="footer__text">© 2020 Kasa. All rights reserved</p>
         </footer>
     );
 }
